e77a44fc327045219cdc5dfcda2b9577ad0de090
rohani2101 <130126901+rohani2101@users.noreply.github.com>
2023-04-19 18:41:14 +0800

    Add files via upload

---
+++ tugas phyton rohani.py
@@ -0,0 +1,105 @@
+print ("======== TUGAS BIODATA MAHASISWA ========")
+print ("============== ROHAN ====================")
+print ("==========================================")
+ 
+print ("nama        : rohani")
+print ("Nim         : si19220033")
+print ("Alamat      : sade")
+print ("prodi       : sistem informasi")
+print ("semester    : 2")
+print ("ipk         : 3.5")
+print ("==================================")
+print ("nilai_matakuliah") 
+print ("pemrograman       : 85")
+print ("Aljabar_linier    : 85")
+print ("bahasa_indonesia  : 75")
+print ( "=================================")
+
+print ("nama        : nurul aulia asri")
+print ("nim         : SI19220027")
+print ("alamat      : bayan")
+print (" prodi      : sistem informasi")
+print ("semester    : 2")
+print ("ipk         : 3.8")
+print ("=====================================")
+print ("nilai_matakuliah")
+print ("pancasila     : 80")
+print ("struktur data :87")
+print ("basis data    : 92")
+print ("======================================")
+
+print ("nama        : sonia tarandari")
+print ("nim         : SI19220034")
+print ("alamat      : sereneng")
+print (" prodi      : sistem informasi")
+print ("semester    : 2")
+print ("ipk         : 3.6")
+print ("=====================================")
+print ("nilai_matakuliah")
+print ("jaringan komputer   : 85")
+print ("sisitemdigital      : 80")
+print ("manajemen           : 90")
+print ("======================================")
+
+print ("nama        : meliana febrian")
+print ("nim         : SI19220012")
+print ("alamat      : bakan")
+print (" prodi      : sistem informasi")
+print ("semester    : 2")
+print ("ipk         : 3.0")
+print ("=====================================")
+print ("nilai_matakuliah")
+print ("kalkulus             : 75")
+print ("pendidikan agama     : 80")
+print ("prilaku organisasi   : 80")
+print ("======================================")
+
+print ("nama        : meitia ba'diatul azomah")
+print ("nim         : SI19220018")
+print ("alamat      : pringgarate")
+print (" prodi      : sistem informasi")
+print ("semester    : 2")
+print ("ipk         : 4.0")
+print ("=====================================")
+print ("nilai_matakuliah")
+print ("e-bisnis      : 90")
+print ("tata kelola   : 92")
+print ("metologi      : 90")
+print ("======================================")
+
+#list nilai
+list_nilai = [1,2,3,4,5,6,7,8,9,10]
+print(list_nilai)
+#menggunkan eppend
+list_nilai.append(11)
+print(list_nilai)
+#menggunakan insert
+list_nilai.insert(0,"2")
+print(list_nilai)
+#menggunakan len
+list_nilai = [1,2,3,4,5,6,7,8,9,10]
+jumlah_index = len (list_nilai)
+print("=================================================================")
+
+
+print ("==================TUGAS MEMBUAT LIAST PROGRAM ===================")
+list_nilai.insert(4,"97")
+print (list_nilai)
+print("==================================")
+list_nilai= [1,2,3,4,5,6,7,8,9,10]
+last_three = list_nilai[-3:]
+print (last_three)
+print("===============================")
+list_nilai=[1,2,3,4,5,6,7,8,9,10]
+list_nilai.remove(1)
+list_nilai.remove(2)
+print (list_nilai)
+print("================================")
+list_nilai= [1,2,3,4,5,6,7,8,9,10]
+last_seven = list_nilai [-7:]
+print (last_seven)
+print("===============================")
+
+
+
+
